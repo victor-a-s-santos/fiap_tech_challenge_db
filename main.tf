bfeb32a836d059216fd86b9633129c4e1daeebaa
Victor Araujo <victor.a.santanna@gmail.com>
2024-10-01 22:19:41 -0300

    ajustes no terraform

--- main.tf
+++ main.tf
@@ -4,13 +4,15 @@ provider "aws" {
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 2
+  max_allocated_storage = 4
   engine               = "postgres"
-  engine_version       = "13.4"
+  engine_version       = "14"
   instance_class       = "db.t3.micro"
   db_name                 = "db-fiap-tech-challenge"
   username             = "admin"
   password             = "admin123456"
-  parameter_group_name = "default.postgres13"
+  parameter_group_name = "default.postgres14"
+  port     = 5432
   skip_final_snapshot  = true
 
   publicly_accessible = true
