5e8092846482a689e1d9d827ea42daf4b43de911
Victor Araujo <victor.a.santanna@gmail.com>
2024-10-01 22:06:53 -0300

    criacao do banco na aws

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1" # Defina a região desejada
+}
+
+resource "aws_db_instance" "example" {
+  allocated_storage    = 2
+  engine               = "postgres"
+  engine_version       = "13.4"
+  instance_class       = "db.t3.micro"
+  name                 = "db-fiap-tech-challenge"
+  username             = "admin"
+  password             = "admin123456"
+  parameter_group_name = "default.postgres13"
+  skip_final_snapshot  = true
+
+  publicly_accessible = true
+
+  tags = {
+    Name = "terraform-rds-fiap-tech-challenge"
+  }
+}
