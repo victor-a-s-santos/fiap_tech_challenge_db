8c62f1e1780967593ef3e3d00996d5be0676a1e5
Victor Araujo <victor.a.santanna@gmail.com>
2024-11-30 20:45:16 -0300

    habilitando dns

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_support   = true  # Habilitar resolução DNS
+  enable_dns_hostnames = true  # Habilitar nomes de host DNS
   tags = {
     Name = "rds-fiap-vpc"
   }
