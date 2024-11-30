94d02a389de021d6c39bd93c6603a883b830ce6d
Victor Araujo <victor.a.santanna@gmail.com>
2024-11-30 20:23:29 -0300

    ajustes para criar subnet

--- main.tf
+++ main.tf
@@ -1,8 +1,50 @@
 provider "aws" {
-  region = "us-east-1" # Defina a região desejada
+  region = "us-east-1" 
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "rds-fiap-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24" 
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "subnet-a"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.2.0/24" 
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "subnet-b"
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  description = "Subnet group para RDS"
+  subnet_ids = [
+    aws_subnet.subnet_a.id,
+    aws_subnet.subnet_b.id,
+  ]
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
 }
 
 resource "aws_db_instance" "default" {
+  db_subnet_group_name = aws_db_subnet_group.default.name
   engine               = "postgres"
   engine_version       = "14"
   db_name                 = "dbFiapTechChallenge"
